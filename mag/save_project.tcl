6ad6b1bf0e1cd17b59518875006f1be24dfa4cb8
Maximiliam Luppe <maximiliam.luppe@gmail.com>
2026-05-08 17:57:15 -0300

    Last(?) commit of Comparators project

--- mag/save_project.tcl
+++ mag/save_project.tcl
@@ -1,6 +1,7 @@
 # Change the settings below to match your design:
 # ------------------------------------------------
 set TOP_LEVEL_CELL     tt_um_maxluppe_ttsky26a_analog
+# set TOP_LEVEL_CELL     ttsky26a_analog
 
 # Save the layout and export GDS/LEF
 # ----------------------------------
@@ -9,3 +10,4 @@ save ${TOP_LEVEL_CELL}.mag
 gds write ../gds/${TOP_LEVEL_CELL}.gds
 # file mkdir ../lef
 lef write ../lef/${TOP_LEVEL_CELL}.lef -hide -pinonly
+#CELL position 49.18 184.46
